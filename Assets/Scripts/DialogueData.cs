817f0169b3af594432370c010bade815fc5dfb94
DESKTOP-SJ6GBQC\Mert POLAT <mert.polat@sabanciuniv.edu>
2024-11-12 16:43:52 +0300

    created a mock fps controller and implemented the virtual assitant logic

---
+++ Assets/Scripts/DialogueData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogueData", menuName = "DialogueData", order = 1)]
+public class DialogueData : ScriptableObject
+{
+    
+    public string[] dialogue;
+}
