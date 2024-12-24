a1e5ab1ac25a168e950d26d0b3df9f0ea5ec59ac
UtkuuUnlu <utku.unlu@sabanciuniv.edu>
2024-12-24 15:27:17 +0300

    TTS and Storyboard
    Now every dialogue is connected to an audio file. Every time a dialogue is shown a TTS audio file is played along with it.

--- Assets/Assistant/Scripts/VirtualAssitant.cs
+++ Assets/Assistant/Scripts/VirtualAssitant.cs
@@ -8,7 +8,6 @@ public class VirtualAssitant : MonoBehaviour, IInteractable
 {
     public VirtualAssitantPatrolData patrolData;
 
-
     public int currentPatrolIndex = 0;
 
     public DialogueData currentDialogueData;
@@ -17,126 +16,122 @@ public class VirtualAssitant : MonoBehaviour, IInteractable
 
     public bool dialogueActive = false;
 
-
     public float speed;
-
-
 
     public Transform xrRigPosition;
 
     public Transform destinationBody;
 
+    private AudioSource audioSource; // Reference to the AudioSource
 
+    private void Awake()
+    {
+        // Ensure there's an AudioSource component on the GameObject
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
 
     public void OnInteraction()
     {
-      
-        if(currentDialogueData == null){
+        if (currentDialogueData == null)
+        {
             Debug.Log("No dialogue data");
             return;
         }
-            
-                if (currentDialogueIndex < currentDialogueData.dialogue.Length)
-                {
-                    if (currentDialogueIndex == 0)
-                    {
-                        DialogueManager.instance.EnableDialoguePanel();
-                        //do a little jump
-                    }
-                    transform.DOSmoothRewind();
-                    transform.DOJump(transform.position, 0.5f, 1, 0.5f);
-                    AdvanceDialogue();
 
-
-                }
-                else{
-                    DialogueManager.instance.DisableDialoguePanel();
-                    currentDialogueIndex = 0;
-
-                }
-
+        if (currentDialogueIndex < currentDialogueData.dialogue.Length)
+        {
+            if (currentDialogueIndex == 0)
+            {
+                DialogueManager.instance.EnableDialoguePanel();
+                // Do a little jump
+            }
+            transform.DOSmoothRewind();
+            transform.DOJump(transform.position, 0.5f, 1, 0.5f);
+            AdvanceDialogue();
+        }
+        else
+        {
+            DialogueManager.instance.DisableDialoguePanel();
+            currentDialogueIndex = 0;
+        }
     }
-
 
     private void Update()
     {
+        xrRigPosition.position = Camera.main.transform.position;
 
+        if (Mathf.Abs(Camera.main.transform.rotation.eulerAngles.y - xrRigPosition.rotation.eulerAngles.y) > 15)
+        {
+            xrRigPosition.rotation = Quaternion.Lerp(
+                xrRigPosition.rotation, 
+                Quaternion.Euler(0, Camera.main.transform.rotation.eulerAngles.y, 0), 
+                Time.deltaTime * 5.0f
+            );
+        }
 
-            //set target position to the camera position
+        Vector3 targetDir = Camera.main.transform.position - transform.position;
+        targetDir.y = 0;
+        Quaternion targetRotation = Quaternion.LookRotation(targetDir);
 
-            xrRigPosition.position = Camera.main.transform.position;
+        Vector3 destination = destinationBody.position;
 
-            //set rig y rotation to camera y rotation
-                 //only lerp if the difference y axis angle is greater than 15 degrees
-            if(Mathf.Abs(Camera.main.transform.rotation.eulerAngles.y - xrRigPosition.rotation.eulerAngles.y) > 15){
-                xrRigPosition.rotation = Quaternion.Lerp(xrRigPosition.rotation, Quaternion.Euler(0, Camera.main.transform.rotation.eulerAngles.y, 0), Time.deltaTime * 5.0f);
-            }
-            
-            
-      
-            Vector3 targetDir = Camera.main.transform.position - transform.position;
-            targetDir.y = 0;
-            Quaternion targetRotation = Quaternion.LookRotation(targetDir);
-
-            //clamp destination y position to 0 and 2.5
-            Vector3 destination = destinationBody.position;
-            //lerp rotation
-            
-
-       
-            
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 5.0f);
-
-
-            
-
-            //lerp to destination
-            transform.position = Vector3.Lerp(transform.position, destinationBody.position, Time.deltaTime * speed);
-
-            //clamp y position
-            transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, 0, 2.5f), transform.position.z);
-        
-
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 5.0f);
+        transform.position = Vector3.Lerp(transform.position, destinationBody.position, Time.deltaTime * speed);
+        transform.position = new Vector3(
+            transform.position.x,
+            Mathf.Clamp(transform.position.y, 0, 2.5f),
+            transform.position.z
+        );
     }
 
-
-     private void OnTriggerEnter2D(Collider2D other) {
-        
-
-        //if we enter a place of interest, we set out current dialogue data to the dialogue data of the place of interest
-        if(other.gameObject.CompareTag("PlaceOfInterest")){
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PlaceOfInterest"))
+        {
             PlaceOfInterest placeOfInterest = other.gameObject.GetComponent<PlaceOfInterest>();
             currentDialogueData = placeOfInterest.dialogueData;
         }
     }
 
-
-    private void OnTriggerEnter(Collider other) {
-            
-    
-            //if we enter a place of interest, we set out current dialogue data to the dialogue data of the place of interest
-            if(other.gameObject.TryGetComponent(out PlaceOfInterest placeOfInterest)){
-                
-                currentDialogueData = placeOfInterest.dialogueData;
-            }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.TryGetComponent(out PlaceOfInterest placeOfInterest))
+        {
+            currentDialogueData = placeOfInterest.dialogueData;
+        }
     }
 
     public void AdvanceDialogue()
     {
+        if (currentDialogueIndex < currentDialogueData.dialogue.Length)
+        {
+            string dialogueText = currentDialogueData.dialogue[currentDialogueIndex];
+            Debug.Log(dialogueText);
+            DialogueManager.instance.DisplayDialogue(dialogueText);
 
-        string dialogueText = currentDialogueData.dialogue[currentDialogueIndex];
-        Debug.Log(dialogueText);
-        DialogueManager.instance.DisplayDialogue(dialogueText);
+            // Play the corresponding audio clip
+            if (currentDialogueData.audioClips != null && 
+                currentDialogueIndex < currentDialogueData.audioClips.Length &&
+                currentDialogueData.audioClips[currentDialogueIndex] != null)
+            {
+                audioSource.clip = currentDialogueData.audioClips[currentDialogueIndex];
+                audioSource.Play();
+            }
+            else
+            {
+                Debug.LogWarning("No audio clip assigned for this dialogue.");
+            }
 
-        currentDialogueIndex++;
-
-
+            currentDialogueIndex++;
+        }
+        else
+        {
+            DialogueManager.instance.DisableDialoguePanel();
+            currentDialogueIndex = 0;
+        }
     }
-
-    
 }
-
-
-
-
-
